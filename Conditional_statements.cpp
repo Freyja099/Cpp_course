d00906b24fd422cbcd4215ecf3a270f88184384d
Aya <140282858+Freyja099@users.noreply.github.com>
2024-10-22 00:42:26 +0100

    Add files via upload

---
+++ Conditional_statements.cpp
@@ -0,0 +1,60 @@
+// * Session 3 : Conditional Statements
+
+// * If statement : Do something if a condition is true. If not, then don't do it
+// * to add other conditions we use : else if() or just else 
+
+// * Switches : alternative to using many "else if" statements compare one values 
+// * against matching cases
+
+// * switch ( the variable) {
+// *   case condition :
+// *    the result we want;
+// *     break;
+// *   case .......
+// *   default: // in case there's no matching cases
+// *   ..............;
+// *     break;
+// *}
+
+// TODO : Calculator program :
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double a , b ;
+    double result;
+    char operation;
+    cout << " Enter the first number : " ;
+    cin >> a;
+    cout << "the second number : " ;
+    cin >> b;
+    cout << " What operation do you want (only the symbole) ? " ;
+    cin >> operation ;
+    switch (operation)
+    {
+        case '+' :
+        result = a + b ;
+        break;
+        case '-' :
+        result = a - b ;
+        break;
+        case '/' : 
+        result = a /b ;
+        break;
+        case '*' :
+        result = a*b;
+        break;
+        default :
+        cout << " Use only the following symbole : + - / *";
+        break;
+    }
+
+    cout << " The result is : " << result ;
+    return 0 ; 
+}
+
+// * Ternary operator = replacement to an if / else statement 
+// * condition? expression 1 : expression 2;
+
