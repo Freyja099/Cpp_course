6b2fbfc97ee2ccec11a46b8c5e56639116fdf7e9
Aya <140282858+Freyja099@users.noreply.github.com>
2024-11-08 15:14:55 +0100

    Add files via upload

---
+++ Pass_value_reference.cpp
@@ -0,0 +1,51 @@
+// * Session 12 : Pass by Value or pass by reference
+
+#include <iostream>
+
+using namespace std;
+
+void swap(string x , string y);
+void swap2(string &x , string &y);
+int main()
+{
+    string x = "this is x now";
+    string y = "this is y now";
+
+    swap(x, y);
+
+    cout << "************************************"<<endl;
+    cout << "x after swap : " << x << endl;
+    cout << "y after swap : " << y << endl;
+
+    cout << "************************************"<<endl;
+    swap2(x, y);
+
+    // * If we display the variables in the function they will be switched
+    // * but if we display them outside the function they will still remain the same
+
+    // * That's because normally when we pass a variable to a function we're passing by value, when we invoke the function we're creating copies of the original ones
+
+    // * To solve this problem we need to pass the variable by reference, we should add a prefixt to the parameters & to pass the variable by reference
+
+    return 0;
+}
+
+void swap(string x , string y)
+{
+    string temp;
+    temp = x;
+    x = y ;
+    y = temp;
+    cout << "x after swap : " << x << endl;
+    cout << "y after swap : " << y << endl;
+}
+
+void swap2(string &x , string &y)
+{
+    string temp;
+    temp = x;
+    x = y ;
+    y = temp;
+    cout << "x after swap : " << x << endl;
+    cout << "y after swap : " << y << endl;
+}
