20f37fbc52f06277d0139b5e422810c077c72c68
Aya <140282858+Freyja099@users.noreply.github.com>
2024-11-08 15:28:24 +0100

    Update and rename to Value_reference_const.cpp

--- Value_reference_const.cpp
+++ Value_reference_const.cpp
@@ -1,4 +1,4 @@
-// * Session 12 : Pass by Value or pass by reference
+// * Session 12 : Pass by Value or pass by reference ; const parameters:
 
 #include <iostream>
 
@@ -49,3 +49,11 @@ void swap2(string &x , string &y)
     cout << "x after swap : " << x << endl;
     cout << "y after swap : " << y << endl;
 }
+
+
+// * ** const parameters 
+
+// * const parameter : parameter that is effectively read-only ; we use the const parameter for couple of reason 
+// * code is more secure & conveys intent also it's useful for references and pointers
+
+// * To use a const parameter we have to add const infront of the parameter
