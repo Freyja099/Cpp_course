82539b66c6d41da17eb41f97ed869e98d1cb184c
Aya <140282858+Freyja099@users.noreply.github.com>
2024-11-09 02:23:20 +0100

    Update Pointers.cpp

--- Pointers.cpp
+++ Pointers.cpp
@@ -29,3 +29,11 @@ int main()
 
     return 0;
 }
+
+
+// * Null value = is a special value that means something has no value
+// * When a pointer is holding a null value, that pointer is not pointing to anything (null pointer)
+
+// * nullptr = keyword represents a null pointer literal
+
+// * nullptrs are helpful when determining if an address was successfully assigned to a pointer
